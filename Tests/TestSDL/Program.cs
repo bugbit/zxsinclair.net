75c8235e93f5e887fd36c6a8dc2e256a784c0de3
bugbit <ohernandezba@gmail.com>
2016-09-10 01:31:55 +0200

    Pruebas SDL

--- Tests/TestSDL/Program.cs
+++ Tests/TestSDL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SDL2;
 
 namespace TestSDL
 {
@@ -6,7 +7,68 @@ namespace TestSDL
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) != 0)
+			{
+				Console.WriteLine($"SDL_Init Error: {SDL.SDL_GetError()}");
+				return ;
+			}
+
+			IntPtr win = SDL.SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+			if (win == IntPtr.Zero)
+			{
+				Console.WriteLine($"SDL_CreateWindow Error: {SDL.SDL_GetError()}");
+				SDL.SDL_Quit();
+				return ;
+			}
+
+			IntPtr ren = SDL.SDL_CreateRenderer(win, -1,SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+			if (ren == IntPtr.Zero){
+				SDL.SDL_DestroyWindow(win);
+				Console.WriteLine($"SDL_CreateRenderer Error: {SDL.SDL_GetError()}");
+				SDL.SDL_Quit();
+				return;
+			}
+
+			
+			//IntPtr namep = SDL2.LPUtf8StrMarshaler.GetInstance().MarshalManagedToNative ("hello.bmp");
+			IntPtr bmp = SDL.SDL_LoadBMP("hello.bmp");
+
+			//SDL2.LPUtf8StrMarshaler.GetInstance ().CleanUpNativeData (namep);
+			if (bmp == IntPtr.Zero){
+				SDL.SDL_DestroyRenderer(ren);
+				SDL.SDL_DestroyWindow(win);
+				Console.WriteLine($"SDL_LoadBMP Error: {SDL.SDL_GetError()}");
+				SDL.SDL_Quit();
+				return;
+			}
+		
+			IntPtr tex = SDL.SDL_CreateTextureFromSurface(ren, bmp);
+			SDL.SDL_FreeSurface(bmp);
+			if (tex == IntPtr.Zero){
+				SDL.SDL_DestroyRenderer(ren);
+				SDL.SDL_DestroyWindow(win);
+				Console.WriteLine($"SDL_CreateTextureFromSurface Error: {SDL.SDL_GetError()}");
+				SDL.SDL_Quit();
+				return;
+			}
+
+			//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+			for (int i = 0; i < 3; ++i)
+			{
+				//First clear the renderer
+				SDL.SDL_RenderClear(ren);
+				//Draw the texture
+				SDL.SDL_RenderCopy(ren, tex, IntPtr.Zero, IntPtr.Zero);
+				//Update the screen
+				SDL.SDL_RenderPresent(ren);
+				//Take a quick break after all that hard work
+				SDL.SDL_Delay(1000);
+			}
+
+			SDL.SDL_DestroyTexture(tex);
+			SDL.SDL_DestroyRenderer(ren);
+			SDL.SDL_DestroyWindow(win);
+			SDL.SDL_Quit();
 		}
 	}
 }
