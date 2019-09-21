501b9793cb8b89ee24666b8ced413fe16d875d12
bugbit <ohernandezba@gmail.com>
2019-09-21 10:27:56 +0200

    TestHighPerformanceTimer

--- Tests/TestHighPerformanceTimer/Program.cs
+++ Tests/TestHighPerformanceTimer/Program.cs
@@ -13,29 +13,43 @@ namespace TestHighPerformanceTimer
 
         static void Main(string[] args)
         {
-            //SerialPi();
-            //SerialPi();
-            //SerialPi();
+
+            /*
+                La precisión de 15 ms (en realidad puede ser de 15-25 ms) se basa en la resolución del temporizador de Windows 55 Hz / 65 Hz. Este es también el período básico de TimeSlice.
+                El valor del reloj del sistema que DateTime.Nowlee solo se actualiza cada 15 ms aproximadamente (o 10 ms en algunos sistemas), razón por la cual los tiempos se cuantifican alrededor de esos intervalos. Hay un efecto de cuantificación adicional que resulta del hecho de que su código se está ejecutando en un sistema operativo multiproceso y, por lo tanto, hay tramos donde su aplicación no está "viva" y, por lo tanto, no mide el tiempo actual real.
+             */
+
+            SerialPi();
+            SerialPi();
+            SerialPi();
+
+           
             var x = HighResolutionDateTime.IsAvailable;
 
             var pHighResolutionDateTime = HighResolutionDateTime.UtcNow;
 
-            Thread.Sleep(314);
+            //Thread.Sleep(314);
+            SerialPi();
 
             var pHighResolutionDateTimeDiff = HighResolutionDateTime.UtcNow - pHighResolutionDateTime;
 
             var pStopwatch = Stopwatch.StartNew();
 
-            Thread.Sleep(314);
+            //Thread.Sleep(314);
+            SerialPi();
 
             pStopwatch.Stop();
+
+            var pTicksD1 = DateTime.UtcNow.Ticks;
+
+            SerialPi();
+
+            var pTicksD2 = DateTime.UtcNow.Ticks;
 
             //SerialPi();
             //var xx = 1;
             //for (var i = 4; i > 0; i--)
             //    xx += i;
-
-
 
 
 
@@ -60,6 +74,15 @@ namespace TestHighPerformanceTimer
             Console.WriteLine("---------------------------------");
             Console.WriteLine($"SerialPi elapse ms: {pHighResolutionDateTimeDiff.Ticks / (double)TimeSpan.TicksPerMillisecond}");
             Console.WriteLine($"SerialPi elapse ns: {pHighResolutionDateTimeDiff.Ticks / ((double)TimeSpan.TicksPerMillisecond / (1000L * 1000L))}");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("TimeDate");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"SerialPi elapse ms: {(pTicksD2-pTicksD1) / (double)TimeSpan.TicksPerMillisecond}");
+            Console.WriteLine($"SerialPi elapse ns: {(pTicksD2 - pTicksD1) / ((double)TimeSpan.TicksPerMillisecond / (1000L * 1000L))}");
             Console.WriteLine("---------------------------------");
             Console.WriteLine();
 
