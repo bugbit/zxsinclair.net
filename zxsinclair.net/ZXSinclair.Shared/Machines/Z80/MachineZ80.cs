785b8b21e178f0ae39a7b6ed7902ecae6d3f314a
bugbit <ohernandezba@gmail.com>
2020-12-04 18:58:33 +0100

    cambios CPU => Machines

---
+++ zxsinclair.net/ZXSinclair.Shared/Machines/Z80/MachineZ80.cs
@@ -0,0 +1,35 @@
+﻿#region LICENSE
+/*
+    ZXSinclair Emulador ZX Computers make in .Net and .Net CORE
+    Copyright (C) 2016 Oscar Hernandez Bano
+    This file is part of ZXSincalir.Net.
+    ZXSincalir.Net is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZXSinclair.Machines.Z80
+{
+    // http://www.z80.info/zip/z80cpu_um.pdf
+    // http://www.z80.info/zip/z80-documented.pdf
+    // https://worldofspectrum.org/z88forever/dn327/z80undoc.htm
+    // https://worldofspectrum.org/faq/resources/documents.htm
+
+    public class MachineZ80 : Machine
+    {
+        protected Regs mRegs = new Regs();
+
+        public Regs Regs => mRegs;
+    }
+}
