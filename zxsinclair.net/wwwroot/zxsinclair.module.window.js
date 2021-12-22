b09e4b8a7d560d3d701f3271090e1158090d73be
bubgit <ohernandezba@gmail.com>
2021-12-22 19:05:24 +0100

    main page

---
+++ zxsinclair.net/wwwroot/zxsinclair.module.window.js
@@ -0,0 +1,10 @@
+export function getWindowSize() {
+    var win = window,
+        doc = document,
+        docElem = doc.documentElement,
+        body = doc.getElementsByTagName('body')[0],
+        x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+        y = win.innerHeight || docElem.clientHeight || body.clientHeight;
+
+    return { width: x, height: y }
+}
